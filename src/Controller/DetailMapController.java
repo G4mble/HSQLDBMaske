ce2f3209302fa1fe07b4e14f803938aae6361645
G4mble <tomone33@web.de>
2014-05-23 20:12:54 +0200

    #detailMapController

--- src/Controller/DetailMapController.java
+++ src/Controller/DetailMapController.java
@@ -33,7 +33,7 @@ public class DetailMapController implements ActionListener
     public void addElement()
     {
         String name = this.detailMapView.getTxtfName().getText();
-        String url = "data//img//detail//" + this.detailMapView.getTxtfUrl().getText();
+        String url = "data//img//map//detail//" + this.detailMapView.getTxtfUrl().getText();
         String pos = this.detailMapView.getTxtrPositionen().getText();
         this.programController.getXmlWriter().addDetailMap(name, url, pos);
     }
